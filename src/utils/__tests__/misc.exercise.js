0d3c4728a30de3b8c0dfe717b41a11ee9623c2ec
colineckert <colineckert10@gmail.com>
2024-01-14 06:52:01 -0800

    formate date test

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,8 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+test('formatDate formats the date to look nice', () => {
+  let date = new Date('2024-01-20')
+  expect(formatDate(date)).toBe('Jan 24')
+  date = new Date('March 22, 1991')
+  expect(formatDate(date)).toBe('Mar 91')
+})
